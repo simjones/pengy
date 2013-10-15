298e1a582a0b6bc0147e49dce2ad841184a12616
DMazak <drmazak@gmail.com>
2013-10-15 16:48:46 -0400

    get_active_projects function/project_post class
    Created project_post class, get_active_projects.

---
+++ get_active_projects.php
@@ -0,0 +1,23 @@
+<?php
+include 'database_func.php';
+/*
+$connection = mysql_connect('localhost', 'drmazak_die', 'diedie') or die ("<p class='error'>Sorry, we were unable to connect to the database server.</p>");
+$database = "drmazak_blog";
+mysql_select_db($database, $connection) or die ("<p class='error'>Sorry, we were unable to connect to the database.</p>");
+*/ 
+function GetActiveProjects($inStatus=null)
+{
+	if (!empty($inStatus))
+	{
+		$query = mysql_query("SELECT * FROM projects ORDER BY id DESC"); 
+	}
+	
+	$postArray = array();
+	while ($row = mysql_fetch_assoc($query))
+	{
+		$myPost = new ProjectPost($row["id"], $row['projectName'], $row['organizer'], $row['dateStart'], $row["dateEnd"], $row['assignTo'], $row['comments'], $row['status']);
+		array_push($postArray, $myPost);
+	}
+	return $postArray;
+}
+?>
---
+++ project_post.php
@@ -0,0 +1,56 @@
+<?php
+
+class ProjectPost
+{
+
+public $id;
+public $projectName;
+public $organizer;
+public $dateStarted;
+public $dateEnded;
+public $assignTo;
+public $comments;
+
+function __construct($inId=null, $inProjectName=null, $inOrganizer $inDateStart=null, $inDateEnd=null, $inAssignTo=null, $inComments=null, $inStatus=null)
+{
+	if (!empty($inId))
+	{
+		$this->id = $inId;
+	}
+	if (!empty($inProjectName))
+	{
+		$this->projectName = $inProjectName;
+	}
+	
+	if (!empty($inOrganizer))
+	{
+		this->organizer = $inOrganizer
+	}
+	
+	if (!empty($inDateStart))
+	{
+		$splitDate = explode("-", $inDateStart);
+		$this->dateStarted = $splitDate[1] . "/" . $splitDate[2] . "/" . $splitDate[0];
+	}
+	
+	if (!empty($inDateEnd))
+	{
+		$splitDate = explode("-", $inDateEnd);
+		$this->dateEnded = $splitDate[1] . "/" . $splitDate[2] . "/" . $splitDate[0];
+	}
+
+	if (!empty($inAssignTo))
+	{
+		this->organizer = $inOrganizer
+	}
+	
+	if (!empty($inComments))
+	{
+		this->comments = $inComments
+	}
+
+}
+
+}
+
+?>
