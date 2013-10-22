2c87a1a9a85d520fb9073dcf7c41d9cb2b79e465
DMazak <drmazak@gmail.com>
2013-10-22 18:11:41 -0400

    date class created
    Began writing date class to handle dates.

---
+++ date.php
@@ -0,0 +1,54 @@
+<?php
+
+class Date 
+{
+	public static function isPossible($date1, $date2=null){
+	// date 1 should be start. date 2 should be end
+	// convert dates to julian
+	// return true if possible
+	// else return false
+	// The date we receive is going to be delimited by a "-"
+	// And will be in the format YYYY-MM-DD
+	// Using the explode method, year will be [0], month [1], date [3]
+	
+	$dateOneExploded = explode("-", $date1);
+	$dateTwoExploded = explode("-", $date2);
+	/*
+	foreach ($dateOneExploded as &$value) {
+		echo $value;
+		}
+		*/
+	$dateOneJulian = gregoriantojd($dateOneExploded[1], $dateOneExploded[2], $dateOneExploded[0]);
+	$dateTwoJulian = gregoriantojd($dateTwoExploded[1], $dateTwoExploded[2], $dateTwoExploded[0]); 	
+	
+	if (dateTwoJulian > dateOneJulian) {
+	return true;
+	}
+	else {
+	return false;
+	}
+	}
+	
+	public static function numDaysBetween($date1, $date2){
+	// calculate the number of days between two dates. use julian dates?
+	$date1 = strtotime($date1);
+	$date2 = strtotime($date2);
+	
+	$datediff = abs($date1 - $date2);
+	
+	return $datediff;
+	
+	}
+	
+	public static function numDaysFromNow($date1) {
+	// calculate the number of days from today to a date in the future.
+	// if date is from past, return negative number
+	}
+	
+}
+	/*
+	calling a static function: Date::isPossible(arg);
+	int gregoriantojd ( int $month , int $day , int $year )
+	string jdtogregorian ( int $julianday )
+	
+	*/
